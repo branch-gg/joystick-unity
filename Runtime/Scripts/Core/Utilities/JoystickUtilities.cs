2047ccdb99d7b11610a8f6c7cd45021107ec38f0
Charles <me@charlesvien.com>
2024-12-11 11:23:52 -0800

    Update building array string and encoding in GetConfigContentAPIUrl

--- Runtime/Scripts/Core/Utilities/JoystickUtilities.cs
+++ Runtime/Scripts/Core/Utilities/JoystickUtilities.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using JoystickRemoteConfig.Core.Data;
 using UnityEngine;
@@ -27,8 +28,9 @@ namespace JoystickRemoteConfig.Core
 
             for (int i = 0; i < contentIds.Length; i++)
             {
+                // Ensure each content ID is properly encoded
                 stringBuilder.Append("\"");
-                stringBuilder.Append(contentIds[i]);
+                stringBuilder.Append(Uri.EscapeDataString(contentIds[i]));
                 stringBuilder.Append("\"");
 
                 if (i < contentIds.Length - 1)
@@ -43,7 +45,10 @@ namespace JoystickRemoteConfig.Core
             string responseTypeParam = "&responseType=serialized";
             string appendParam = shouldSerialized ? responseTypeParam : string.Empty;
 
-            return $"https://api.getjoystick.com/api/v1/combine/?c=[{stringBuilder}]&dynamic=true{appendParam}";
+            // Properly encode the entire 'c=[...]' query parameter
+            string encodedCParameter = Uri.EscapeDataString($"[{stringBuilder}]");
+
+            return $"https://api.getjoystick.com/api/v1/combine/?c={encodedCParameter}&dynamic=true{appendParam}";
         }
 
         public static string GetCatalogAPIUrl()
